54f0ec4a976f19293bb0927d2b7c32557207e642
Peter Esan <peteresan567@gmail.com>
2023-02-06 15:26:27 +0100

    Initial commit

---
+++ src/main/java/com/pcechz/mke_bank/MkeBankApplication.java
@@ -0,0 +1,13 @@
+package com.pcechz.mke_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkeBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MkeBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pcechz/mke_bank/MkeBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcechz.mke_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkeBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
